49b2060272494802d565cf2353f2f02032506123
mrChernicharo <felipe.chernicharo@gmail.com>
2020-04-30 14:49:06 -0300

    toasts => estilos prontos

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -6,7 +6,32 @@ import { Container, Toast } from './styles';
 const ToastContainer: React.FC = () => {
   return (
     <Container>
-      <Toast>
+      <Toast hasDescription>
+        <FiAlertCircle size={20} />
+
+        <div>
+          <strong>Aconteceu um Erro</strong>
+          <p>Não foi possível logar na aplicação</p>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+
+      <Toast type="success" hasDescription={false}>
+        <FiAlertCircle size={20} />
+
+        <div>
+          <strong>Aconteceu um Erro</strong>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+
+      <Toast type="error" hasDescription>
         <FiAlertCircle size={20} />
 
         <div>
