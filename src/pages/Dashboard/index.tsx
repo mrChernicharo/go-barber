98dfe7fe9743d70b0a08237600e0fbd6768a8cde
mrChernicharo <felipe.chernicharo@gmail.com>
2020-05-28 14:34:22 -0300

    botao logout no header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { FiPower } from 'react-icons/fi';
+import { useAuth } from '../../hooks/auth';
 import { Container, Header, HeaderContent, Profile } from './styles';
 import logoImg from '../../assets/go_barber_logo.svg';
 
 const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
   return (
     <Container>
       <Header>
@@ -15,13 +18,13 @@ const Dashboard: React.FC = () => {
               src="https://avatars0.githubusercontent.com/u/52944886?s=400&u=848be9ef8d675be4453d7254cdeae48bfcfe848a&v=4"
               alt="Felipe Chernicharo"
             />
+            <div>
+              <span>Bem-vindo</span>
+              <strong>Felipe Chernicharo</strong>
+            </div>
           </Profile>
-          <div>
-            <span>Bem-vindo</span>
-            <p>Felipe Chernicharo</p>
-          </div>
 
-          <button type="button">
+          <button type="button" onClick={signOut}>
             <FiPower />
           </button>
         </HeaderContent>
--- src/pages/Dashboard/styles.tsx
+++ src/pages/Dashboard/styles.tsx
@@ -1,7 +1,58 @@
-import styed from 'styled-components';
+import styled from 'styled-components';
 
-export const Container = styed.div``;
+export const Container = styled.div``;
 
-export const Header = styed.header``;
-export const HeaderContent = styed.div``;
-export const Profile = styed.div``;
+export const Header = styled.header`
+  padding: 32px 0;
+  background: #28262e;
+`;
+
+export const HeaderContent = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+
+  /**selecionar a primeira imagem */
+  > img {
+    height: 80px;
+  }
+
+  button {
+    margin-left: auto;
+    background: transparent;
+    border: 0;
+
+    svg {
+      color: #999591;
+      height: 20px;
+      width: 20px;
+    }
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 80px;
+
+  img {
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+    line-height: 24px;
+
+    span {
+      color: #f4ede8;
+    }
+    strong {
+      color: #ff9000;
+    }
+  }
+`;
