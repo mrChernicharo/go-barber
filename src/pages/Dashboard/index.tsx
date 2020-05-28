ca8a18d05916e3ecda61edd5eaecb5bd960b9f5a
mrChernicharo <felipe.chernicharo@gmail.com>
2020-05-28 13:47:29 -0300

    iniciando layout dashboard ->  header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { Container } from '../SignIn/styles';
+import { FiPower } from 'react-icons/fi';
+import { Container, Header, HeaderContent, Profile } from './styles';
+import logoImg from '../../assets/go_barber_logo.svg';
 
 const Dashboard: React.FC = () => {
   return (
     <Container>
-      <h1>Dashboard</h1>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img
+              src="https://avatars0.githubusercontent.com/u/52944886?s=400&u=848be9ef8d675be4453d7254cdeae48bfcfe848a&v=4"
+              alt="Felipe Chernicharo"
+            />
+          </Profile>
+          <div>
+            <span>Bem-vindo</span>
+            <p>Felipe Chernicharo</p>
+          </div>
+
+          <button type="button">
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 };
---
+++ src/pages/Dashboard/styles.tsx
@@ -0,0 +1,7 @@
+import styed from 'styled-components';
+
+export const Container = styed.div``;
+
+export const Header = styed.header``;
+export const HeaderContent = styed.div``;
+export const Profile = styed.div``;
