ad05177b840c2b1594834c14a7ba7f2720e6c4d1
mrChernicharo <felipe.chernicharo@gmail.com>
2020-05-28 02:04:16 -0300

    recuperacao de senha ok

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useRef } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
@@ -15,6 +15,7 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content, AnimationContainer, Background } from './styles';
+import api from '../../services/api';
 
 interface ResetPasswordFromData {
   password: string;
@@ -25,6 +26,9 @@ const ResetPassword: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const { addToast } = useToast();
   const history = useHistory();
+  const location = useLocation();
+
+  console.log(location);
 
   const handleSubmit = useCallback(
     async (data: ResetPasswordFromData) => {
@@ -43,11 +47,24 @@ const ResetPassword: React.FC = () => {
           abortEarly: false,
         });
 
-        history.push('/signin');
+        const { password, password_confirmation } = data;
+        const token = location.search.replace('?token=', '');
+
+        if (!token) {
+          throw new Error();
+        }
+
+        await api.post('/password/reset', {
+          password,
+          password_confirmation,
+          token,
+        });
+
+        history.push('/');
 
         addToast({
           type: 'success',
-          title: 'Cadastro Realizado!',
+          title: 'Senha atualizada com sucesso!',
           description: 'Você já pode fazer seu login!',
         });
       } catch (err) {
@@ -66,7 +83,7 @@ const ResetPassword: React.FC = () => {
         });
       }
     },
-    [addToast, history],
+    [addToast, history, location.search],
   );
 
   return (
